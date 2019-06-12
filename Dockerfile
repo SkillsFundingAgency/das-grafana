e9041783a7d5d61b1b5b4efae89c9084173bf307
Craig Gumbley <craiggumbley@gmail.com>
2019-06-12 20:50:45 +0100

    Tidy up repo

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM grafana/grafana:latest
-
-COPY ./datasources/azure.datasource.yml /etc/grafana/provisioning/datasources/azure.datasource.yml
-COPY ./custom.ini /etc/grafana/grafana.ini
-
-ENV GF_INSTALL_PLUGINS=grafana-azure-monitor-datasource
--- helm/tiller/tiller-setup.ps1
+++
@@ -1,8 +0,0 @@
-# --- Deploy tiller service account
-kubectl create -f tiller-sa.yaml
-
-# --- initi helm and tiller if it has already been installed
-helm.exe init --service-account tiller --history-max 200
-
-# --- OR if tiller is already installed on the cluster, just run
-$null = helm init --client-only
