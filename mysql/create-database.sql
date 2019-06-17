5ecbe2e7817686f03f45a892c8a26b21fe137f96
Craig Gumbley <craiggumbley@gmail.com>
2019-06-17 12:13:12 +0000

    remove db creation

--- mysql/create-database.sql
+++ mysql/create-database.sql
@@ -1,4 +1,3 @@
-CREATE DATABASE [IF NOT EXISTS] __databaseName__
 CREATE USER IF NOT EXISTS '__MySqlServiceAccountUsername__' IDENTIFIED BY '__MySqlServiceAccountPassword__';
 GRANT ALL PRIVILEGES ON * .__databaseName__ TO '__MySqlServiceAccountUsername__';
 FLUSH PRIVILEGES;
