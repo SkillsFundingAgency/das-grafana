140c106e6cbd57963b032352e8fceb2e86419c32
Craig Gumbley <craiggumbley@gmail.com>
2019-06-17 12:39:18 +0000

    revert

--- mysql/create-database.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER IF NOT EXISTS '__MySqlServiceAccountUsername__' IDENTIFIED BY '__MySqlServiceAccountPassword__';
-GRANT ALL PRIVILEGES ON * .__databaseName__ TO '__MySqlServiceAccountUsername__';
-FLUSH PRIVILEGES;
