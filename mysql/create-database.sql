b015ff182f2c0e278651170185525c8d1d5436be
Craig Gumbley <craiggumbley@gmail.com>
2019-06-17 11:08:14 +0000

    add database creation script

---
+++ mysql/create-database.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE [IF NOT EXISTS] __databaseName__
+CREATE USER IF NOT EXISTS '__MySqlServiceAccountUsername__' IDENTIFIED BY '__MySqlServiceAccountPassword__';
+GRANT ALL PRIVILEGES ON * .__databaseName__ TO '__MySqlServiceAccountUsername__';
+FLUSH PRIVILEGES;
