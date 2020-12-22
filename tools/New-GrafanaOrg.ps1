f89f7663e9b84652ac79f095093872389f18b843
NickGraham101 <nick.graham101@googlemail.com>
2020-12-22 16:27:33 +0000

    Modified New-GrafanaOrg to process an array of names

--- tools/New-GrafanaOrg.ps1
+++ tools/New-GrafanaOrg.ps1
@@ -8,11 +8,8 @@ Create a Grafana organisation if it doesn't exist.
 .PARAMETER GrafanaBaseUri
 The base uri of the grafana instance.
 
-.PARAMETER OrgId
-The Id of the organisation to create, used to check if the organisation already exists
-
-.PARAMETER OrgName
-The Name of the organisation.  Rerunning the script with a different value will not update the organisation name.
+.PARAMETER OrgNames
+An array of names that will be added to the organisation.  Rerunning the script with a different value will add new organisations, it will not update the organisation name.
 
 .PARAMETER Password
 The password of a Grafana admin account
@@ -21,16 +18,14 @@ The password of a Grafana admin account
 The username of a Grafana admin account
 
 .EXAMPLE
-./New-GrafanaOrg.ps1 -GrafanaBaseUri https://foo.grafana.gov.uk -OrgId 2 -OrgName "Foo Service" -Password not-a-real-password -Username admin
+./New-GrafanaOrg.ps1 -GrafanaBaseUri https://foo.grafana.gov.uk -OrgName "Foo Service", "Bar Service" -Password not-a-real-password -Username admin
 #>
 [CmdletBinding()]
 param(
     [Parameter(Mandatory = $true)]    
     [string]$GrafanaBaseUri,
     [Parameter(Mandatory = $true)]
-    [int]$OrgId,
-    [Parameter(Mandatory = $true)]
-    [string]$OrgName,
+    [string[]]$OrgNames,
     [Parameter(Mandatory = $true)]
     [string]$Password,
     [Parameter(Mandatory = $true)]
@@ -41,14 +36,16 @@ $Base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0
 $Headers = @{Authorization=("Basic {0}" -f $Base64AuthInfo)}
 
 $ExistingOrgs = Invoke-RestMethod -Uri $GrafanaBaseUri/api/orgs -Method GET -Headers $Headers
-if($ExistingOrg = $ExistingOrgs | Where-Object { $_.id -eq $OrgId }) {
-    Write-Output "Organisation $($ExistingOrg.name) with id $OrgId already exists."
-}
-else {
-    Write-Output "Organisation $OrgId doesn't exist, creating ..."
-    $Body = @{
-        id = $OrgId
-        name = $OrgName
+foreach ($OrgName in $OrgNames) {
+    if($ExistingOrg = $ExistingOrgs | Where-Object { $_.name -eq $OrgName }) {
+        Write-Output "Organisation $OrgName with id $($ExistingOrg.id) already exists."
     }
-    Invoke-RestMethod -Uri $GrafanaBaseUri/api/orgs -Method POST -Body $Body -Headers $Headers
+    else {
+        Write-Output "Organisation $OrgName doesn't exist, creating ..."
+        $Body = @{
+            name = $OrgName
+        }
+        Invoke-RestMethod -Uri $GrafanaBaseUri/api/orgs -Method POST -Body $Body -Headers $Headers
+    }
 }
+
